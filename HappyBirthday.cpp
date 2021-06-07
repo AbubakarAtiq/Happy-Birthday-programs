72753f471683d0fa54a7673a63fdabb1e59d7a38
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-06-07 23:44:06 +0500

    Add files via upload

---
+++ HappyBirthday.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include <system_error>
+//use instead of #include<windows.h>
+using namespace std;
+int main()
+{
+    for(;;)
+    {
+        system("color 02");
+        cout<<"\t\tHappy Birthday:Hasan Bhai"<<endl;
+        system("color 03");
+        system("color 04");
+        system("color 05");
+        system("color 06");
+        system("color 07");
+    }
+    return 0;
+}
